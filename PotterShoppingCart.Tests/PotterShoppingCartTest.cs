3db62b5da737d5142c7db486b264ea53fec53da1
Kuei-PC\Kuei <ragnakuei@livemail.tw>
2017-01-16 14:44:05 +0800

    第二個紅燈

--- PotterShoppingCart.Tests/PotterShoppingCartTest.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -33,5 +33,26 @@ namespace PotterShoppingCart.Tests
             decimal expected = 100;
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Payment_第一集買一本加第二集買一本_應為190()
+        {
+            // arrange
+            var target = new ShoppingCart();
+            var shoppingCartItems = new List<ShoppingCartItem> {
+                new ShoppingCartItem(1,"哈利波特第一集",1),
+                new ShoppingCartItem(2,"哈利波特第二集",1),
+                new ShoppingCartItem(3,"哈利波特第三集",0),
+                new ShoppingCartItem(4,"哈利波特第四集",0),
+                new ShoppingCartItem(5,"哈利波特第五集",0),
+            };
+
+            // act:結帳
+            decimal actual = target.CheckOut(shoppingCartItems);
+
+            // assert
+            decimal expected = 190;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
